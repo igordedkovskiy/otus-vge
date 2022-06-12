07361deec1d4a29409a71e18c16864eeea9332de
Igor Dedkovskiy <divmy@yandex.ru>
2022-06-12 10:35:06 +0000

    Generate docs

---
+++ docs/html/search/all_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['homework_2d04_3a_20otus_2dvge_18',['homework-04: otus-vge',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ docs/html/search/all_9.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['m_5fpoints_20',['m_points',['../classvge__model_1_1_shape.html#aa4c0ee2ea525ad23c86aa5452bfec4a2',1,'vge_model::Shape::m_points()'],['../classvge__view_1_1_shape.html#aac683a22091bab16093236f9c6be0aca',1,'vge_view::Shape::m_points()']]],
+  ['main_21',['main',['../main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.cpp']]],
+  ['main_2ecpp_22',['main.cpp',['../main_8cpp.html',1,'']]],
+  ['mainwindow_23',['MainWindow',['../classvge__controller_1_1_main_window.html',1,'vge_controller::MainWindow'],['../classvge__controller_1_1_main_window.html#aa98df7248c1e0e4a1fc4ad70353229a6',1,'vge_controller::MainWindow::MainWindow()']]],
+  ['model_24',['Model',['../classvge__model_1_1_model.html',1,'vge_model']]],
+  ['model_2ecpp_25',['model.cpp',['../model_8cpp.html',1,'']]],
+  ['model_2ehpp_26',['model.hpp',['../model_8hpp.html',1,'']]]
+];
